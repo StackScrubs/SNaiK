88d1aae1dfe78990aa8c70314c4b3e1f76656974
Magnus Steensland <mag.steensland@gmail.com>
2022-11-09 16:40:13 +0100

    created basic q table, dqnagent and memory
    Co-authored-by: chrisryda <chrisryda@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from snake_env import SnakeEnv
+from dqn_agent import DQNAgent
 import random
 
 env = SnakeEnv(render_mode='human', size=8)
 terminated, truncated = False, False
 
+agent = DQNAgent()
+
 while True:
-    action = random.choice([0,1,2])
+    # Select action (random or from QTable)
+    action = agent.act()
+
+    # Execute action and recieve output
     observation, reward, terminated, truncated, info = env.step(action)
+
     print((observation, reward, terminated, truncated, info))
     if terminated or truncated:
         print("**RESET**")
         env.reset()
+
+    # agent.memorize(...)
+
+    # agent.replay(...)
+
 
 env.close()
--- replay_memory.py
+++ replay_memory.py
@@ -1,47 +1,18 @@
-
-class DQNAgent:
-    def __init__():
-
-        pass
-    
-    def _build_model():
-        
-        pass
-    
-    def memorize(): 
-        
-        pass
-    
-    def act(): #return an action
-        
-        pass
-    
-    def replay():
-        
-        pass
-
-    def load():
-        
-        pass
-
-    def save():
-        
-        pass
-
+from collections import deque, namedtuple
+from random import sample
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.memory = deque([],maxlen=capacity)
 
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
+    def push(self, state, action, next_state, reward):
+        """Save a transition, and if full, removes the leftmost transition (oldest)"""
+        self.memory.append(Transition(state, action, next_state, reward))
 
     def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
+        return sample(self.memory, batch_size)
 
     def __len__(self):
         return len(self.memory)
