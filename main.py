5aa1a67c5f799b528383640dca174fb4d85d9d1e
norgor <7034394+norgor@users.noreply.github.com>
2022-11-10 17:00:19 +0100

    Removed unused code from main

--- main.py
+++ main.py
@@ -10,22 +10,15 @@ random.seed(SEED)
 
 
 def main():
-    deds = 0
-    best_reward = -100000000000
     while True:
         action = random.randint(0, 2)
         observation, reward, terminated, truncated, info = env.step(action)
-        # print((observation, reward, terminated, truncated, info))
+        print((observation, reward, terminated, truncated, info))
         
         if terminated or truncated:
-            if reward > best_reward:
-                print(f"{deds} REWARD {reward}")
-                best_reward = reward
-            deds += 1
             env.reset(seed=SEED)
 
     env.close()
-
 
 if __name__ == "__main__":
     main()
