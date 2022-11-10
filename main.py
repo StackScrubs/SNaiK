41cd8b09487b575d788626f7262f518b72b3caa4
tomrsae <tommy.saetre@hotmail.com>
2022-11-10 16:13:14 +0100

    reset main
    Co-authored-by: norgor <norgor@users.noreply.github.com>
    Co-authored-by: chrisryda <chrisryda@users.noreply.github.com>
    Co-authored-by: Magnus Steensland <magsteen@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from snake_env import SnakeEnv
-from dqn_agent import DQNAgent
 import random
 
 SEED = 70
@@ -7,22 +6,11 @@ SEED = 70
 env = SnakeEnv(render_mode='human', size=8, seed=SEED)
 terminated, truncated = False, False
 
-agent = DQNAgent()
-
 while True:
-    # Select action (random or from QTable)
-    action = agent.act()
-
-    # Execute action and recieve output
+    action = random.choice([0,1,2])
     observation, reward, terminated, truncated, info = env.step(action)
-
     print((observation, reward, terminated, truncated, info))
     if terminated or truncated:
         env.reset(seed=SEED)
-
-    # agent.memorize(...)
-
-    # agent.replay(...)
-
 
 env.close()
