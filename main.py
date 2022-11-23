112fc62a8ec7d1542cf383e14d7451f0b62e1849
magsteen <mag.steensland@gmail.com>
2022-11-23 14:32:18 +0100

    added file options

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import click
 from typing import Optional
 from enum import Enum
 from utils.ctx import CLIContext, AgentContext, EnvironmentContext
-from utils.option_handlers import RequiredByWhenSetTo
+from utils.option_handlers import RequiredByWhenSetTo, Mutex
+from pynput import keyboard
 
 class DISCRETIZER_TYPES(str, Enum):
     FULL = "full"
@@ -33,35 +34,47 @@ def entry(ctx, alpha, gamma, size, render, seed):
     ctx.obj = CLIContext(agent_ctx, environment_ctx)
 
 @entry.command()
-@click.argument("discretizer", type=click.Choice(DISCRETIZER_TYPES), required=True)
-#rename handler class and args?
+@click.option("-d", "--discretizer", type=click.Choice(DISCRETIZER_TYPES), required=False, cls=Mutex, other="file")
+@click.option("-f", "--file", type=str, required=False, cls=Mutex, other="discretizer")
 @click.option("-ns", "--n-sectors", type=int, required=False, cls=RequiredByWhenSetTo, required_by="discretizer", set_to=DISCRETIZER_TYPES.ANGULAR.value)
 @click.option("-qs", "--quad-size", type=int, required=False, cls=RequiredByWhenSetTo, required_by="discretizer", set_to=DISCRETIZER_TYPES.QUAD.value)
 @click.pass_obj
-def qlearning(ctx, discretizer, n_sectors, quad_size):
+def qlearning(ctx, discretizer, file, n_sectors, quad_size):
     print("TYPE=Q LEARNING")
-    print(f"DISCRETIZER={discretizer}")
-    
     agent = None
     
-    discretizer_obj = None # Full by default (once we merge in classes)
-    if discretizer is DISCRETIZER_TYPES.ANGULAR:
-        print(f"N SECTORS={n_sectors}")
-        # set discretizer_obj (once we merge in classes)
-    elif discretizer is DISCRETIZER_TYPES.QUAD:
-        print(f"QUAD SIZE={quad_size}")
-        # set discretizer_obj (once we merge in classes)
+    if file:
+        print(f"FROM FILE={file}")
+        agent = SnakeQLearningAgent.from_file(file)
+    
+    elif discretizer:
+        print(f"DISCRETIZER={discretizer}")
+        discretizer_obj = None # Full by default (once we merge in classes)
         
-    agent = SnakeQLearningAgent(ctx.env_ctx.size)#ctx.agent_ctx, discretizer_obj)
+        if discretizer is DISCRETIZER_TYPES.ANGULAR:
+            print(f"N SECTORS={n_sectors}")
+            # set discretizer_obj (once we merge in classes)
+        elif discretizer is DISCRETIZER_TYPES.QUAD:
+            print(f"QUAD SIZE={quad_size}")
+            # set discretizer_obj (once we merge in classes)
+            
+        agent = SnakeQLearningAgent(ctx.env_ctx.size)#ctx.agent_ctx, discretizer_obj)
 
     main(agent, ctx.env_ctx)
 
 @entry.command()
+@click.option("-f", "--file", type=str, required=False)
 @click.pass_obj
-def dqn():
+def dqn(ctx, file):
     print("TYPE=DQN")
-
-    #agent = DQNAgent(ctx.agent_ctx)
+    agent = None
+    
+    if file:
+        #agent = DQNAgent.from_file(file)
+        pass
+    else:
+        #agent = DQNAgent(ctx.agent_ctx)
+        pass
 
     #main(agent, ctx.env_ctx)
 
@@ -100,8 +113,30 @@ class AgentRunner:
             if terminated or truncated:
                 self.learning_env.reset()
 
-
 def main(agent, env_ctx):
+    def on_press(key):
+        if not isinstance(key, keyboard.KeyCode):
+            return
+
+        if key.char == "s":
+            # Save agent/model
+            print("Saving current model state...")
+            file = agent.to_file()
+            print(f"Saved model state as \"{file}\".")
+        elif key.char == "g":
+            # Create performance graph/plot
+            print("Creating performance graph of current learning...")
+            
+            # Save graph/plot
+            print("Graph created and saved.")
+        
+        return
+    
+    listener = keyboard.Listener(
+        on_press=on_press
+    )
+    listener.start()
+    
     agent_runner = AgentRunner(agent, env_ctx)
     agent_runner.run()
 
--- utils/option_handlers.py
+++ utils/option_handlers.py
@@ -7,7 +7,7 @@ class RequiredByWhenSetTo(Option):
         assert self.required_by, "'required_by' parameter required"
         assert self.set_to, "'set_to' parameter required"
         kwargs["help"] = (kwargs.get("help", "") +
-            f" NOTE: This argument is required when {self.required_by} is set to {self.set_to}"
+            f" NOTE: This option is required when '{self.required_by}' is set to '{self.set_to}'"
         ).strip()
         super(RequiredByWhenSetTo, self).__init__(*args, **kwargs)
 
@@ -15,10 +15,29 @@ class RequiredByWhenSetTo(Option):
         self_present = self.name in opts
         required_by_set_correct = opts[self.required_by] == self.set_to
 
-        if required_by_set_correct:
-            if not self_present:
-                raise UsageError(
-                    f"Illegal usage: {self.name} is required when {self.required_by} is set to {self.set_to}"
-                )
-
+        if required_by_set_correct and not self_present:
+            raise UsageError(
+                f"Illegal usage: {self.name} is required when {self.required_by} is set to {self.set_to}"
+            )
+                
         return super(RequiredByWhenSetTo, self).handle_parse_result(ctx, opts, args)
+
+class Mutex(Option):
+    def __init__(self, *args, **kwargs):
+        self.other = kwargs.pop("other")
+        assert self.other, "'other' parameter required"
+        kwargs["help"] = (kwargs.get("help", "") +
+            f" NOTE: This option is mutually exclusive with the other option '{self.other}'"
+        ).strip()
+        super(Mutex, self).__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args):
+        self_present = self.name in opts
+        other_present = self.other in opts
+
+        if other_present and self_present:
+            raise UsageError(
+                f"Illegal usage: {self.other} cannot be used with {self.name}"
+            )
+
+        return super(Mutex, self).handle_parse_result(ctx, opts, args)
